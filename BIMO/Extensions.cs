9993ec35491ae09df091c802ea4bba06fc7c34f9
snailya2005 <snailya2005@gmail.com>
2022-07-05 11:23:24 +0800

    feat: auto add a synchronized background shape that can move along with foreground shape

---
+++ BIMO/Extensions.cs
@@ -0,0 +1,76 @@
+﻿using Microsoft.Office.Interop.Visio;
+using System;
+
+namespace BIMO
+{
+	internal static class Extensions
+	{
+		internal static void AppendBackground(this Page page, string name = "")
+		{
+			var app = page.Application;
+
+			var scopeId = app.BeginUndoScope("Insert Page");
+
+			var background = app.ActiveDocument.Pages.Add();
+			background.Background = 1;
+			background.Name = string.IsNullOrEmpty(name) ? $"{page.Name}_bg" : name;
+			page.BackPage = background;
+
+			app.EndUndoScope(scopeId, true);
+		}
+
+		internal static Shape CopyToBackground(this Shape foregndShape, Master master = null)
+		{
+			var app = foregndShape.Application;
+
+			var scopeId = app.BeginUndoScope("Copy To Background");
+
+			// prepare page, master and positon
+			var backpage = foregndShape.ContainingPage.BackPage;
+			if (master == null)
+			{
+				// use the same shape if user not specify the master to use
+				master = foregndShape.Master;
+			}
+			var (positionX, positionY) = (foregndShape.Cells["PinX"].Result[""], foregndShape.Cells["PinY"].Result[""]);
+
+			// place related shape into background page
+			var bkgndShape = backpage.Drop(master, positionX, positionY);
+
+#if DEBUG
+			// this is only for demo reason to highlight the  background shape
+			bkgndShape.Cells["FillForegnd"].FormulaU = "THEMEGUARD(RGB(255, 255, 0))";
+#endif
+
+			// persist background shape id to foreground shape's sheet
+			var rowName = "bkgndShape";
+			var uniqueId = bkgndShape.UniqueID[(short)VisUniqueIDArgs.visGetOrMakeGUID];
+
+			foregndShape.AddNamedRow((short)VisSectionIndices.visSectionUser, rowName, (short)VisRowTags.visTagDefault);
+			foregndShape.Cells[$"User.{rowName}"].Formula = $"\"{uniqueId}\"";
+
+			foregndShape.CellChanged += OnPositionChanged;
+
+			app.EndUndoScope(scopeId, true);
+
+			return bkgndShape;
+		}
+
+		private static void OnPositionChanged(Cell Cell)
+		{
+			if (!Convert.ToBoolean(Cell.Shape.CellExistsU["User.bkgndShape", 1])) return;
+
+			var uniqueId = Cell.Shape.Cells["User.bkgndShape"].ResultStr[""];
+			var bkgndShape = Cell.Shape.ContainingPage.BackPage.Shapes.ItemU[uniqueId];
+
+			if (Cell.Name == "PinX")
+			{
+				bkgndShape.Cells["PinX"].Result[""] = Cell.Result[""];
+			}
+			if (Cell.Name == "PinY")
+			{
+				bkgndShape.Cells["PinY"].Result[""] = Cell.Result[""];
+			}
+		}
+	}
+}
---
+++ BIMO/ThisAddIn.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using Visio = Microsoft.Office.Interop.Visio;
+using Microsoft.Office.Interop.Visio;
+
+namespace BIMO
+{
+	public partial class ThisAddIn
+	{
+		private Visio.Application _visio;
+
+		private void ThisAddIn_Startup(object sender, EventArgs e)
+		{
+			_visio = Globals.ThisAddIn.Application;
+
+			// Register shapeadded event for all newly added shape
+			_visio.ShapeAdded += OnShapeAdded;
+			_visio.BeforeShapeDelete += OnBeforeShapeDeleted;
+		}
+
+		private void OnBeforeShapeDeleted(Shape shape)
+		{
+			// delete bkgndShape  if exist
+			if (Convert.ToBoolean(shape.CellExistsU["User.bkgndShape", 1]))
+			{
+				var uniqueId = shape.Cells["User.bkgndShape"].ResultStr[""];
+				var item = shape.ContainingPage.BackPage.Shapes.ItemU[uniqueId];
+				if (item is Shape bkgndShape)
+					bkgndShape.Delete();
+			}
+		}
+
+		private void OnShapeAdded(Shape shape)
+		{
+			// ignore if current page isn't a foreground page
+			if (Convert.ToBoolean(shape.ContainingPage.Background)) return;
+
+			// add background page if not exist
+			if (shape.ContainingPage.BackPage == null) shape.ContainingPage.AppendBackground();
+
+			// add the related shape into background
+			shape.CopyToBackground();
+
+			// Switch back to frontpage
+			_visio.ActiveWindow.Page = shape.ContainingPage;
+		}
+
+
+		private void ThisAddIn_Shutdown(object sender, System.EventArgs e)
+		{
+			_visio.ShapeAdded -= OnShapeAdded;
+		}
+
+		#region VSTO generated code
+
+		/// <summary>
+		/// Required method for Designer support - do not modify
+		/// the contents of this method with the code editor.
+		/// </summary>
+		private void InternalStartup()
+		{
+			this.Startup += new System.EventHandler(ThisAddIn_Startup);
+			this.Shutdown += new System.EventHandler(ThisAddIn_Shutdown);
+		}
+
+		#endregion
+	}
+}
